a5f8dedc8dca7c2c89d12116d8acd6d1aaeea612
Molly Montgomery <Molly@mollys-air-2.lan>
2022-03-10 22:24:32 -0600

    added folders in schemas and added some routes to server.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,3 @@
+const { AuthenticationError } = require("apollo-server-errors");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
+
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
