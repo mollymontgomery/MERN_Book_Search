5e09d4e9f28e0867bb13116a33badfae6e56d4af
Molly Montgomery <Molly@mollys-air-2.attlocal.net>
2022-03-13 09:28:45 -0500

    worked on savedBooks and installed react-hooks

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,6 @@
 import { gql } from "@apollo/client"
 
-export const QUERY_ME = gql`
+export const GET_ME = gql`
 {
     me {
         _id
@@ -16,4 +16,4 @@ export const QUERY_ME = gql`
         }
     }
 }
-`
+`;
